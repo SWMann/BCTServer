58a8b933245d934c6351fd0098073b1f3b42c47a
SWMann <83301546+SWMann@users.noreply.github.com>
2024-04-27 20:25:33 +0100

    Changes

---
+++ src/qapi.js
@@ -0,0 +1,75 @@
+export default {
+	"pages": {
+		"root": {
+			"name": "root",
+			"children": [
+				"662d4e4bfdcae00021e1b8af",
+				"662d4e4bfdcae00021e1b8b3",
+				"662d4e4bfdcae00021e1b8b7",
+				"662d4e4cfdcae00021e1b8bf",
+				"662d4e4cfdcae00021e1b8bb"
+			],
+			"id": "root",
+			"pageUrl": "root"
+		},
+		"662d4e4bfdcae00021e1b8af": {
+			"id": "662d4e4bfdcae00021e1b8af",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"662d4e4bfdcae00021e1b8b3": {
+			"id": "662d4e4bfdcae00021e1b8b3",
+			"name": "index",
+			"pageUrl": "index",
+			"seo": {
+				"og:description": "It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something bigger. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference.",
+				"title": "Home | Website Example",
+				"og:title": "Home | Website Example",
+				"og:image": "https://uploads.quarkly.io/5f44d0da669357001e60ed14/images/website-example-quarkly.png?v=2020-11-05T19:48:01.806Z",
+				"description": "It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something bigger. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference."
+			}
+		},
+		"662d4e4bfdcae00021e1b8b7": {
+			"id": "662d4e4bfdcae00021e1b8b7",
+			"pageUrl": "about",
+			"name": "About",
+			"seo": {
+				"og:title": "About | Website Example",
+				"og:description": "It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something bigger. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference.",
+				"description": "It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something bigger. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference.",
+				"og:image": "https://uploads.quarkly.io/5f44d0da669357001e60ed14/images/website-example-quarkly.png?v=2020-11-05T19:48:01.806Z",
+				"title": "About | Website Example"
+			}
+		},
+		"662d4e4cfdcae00021e1b8bb": {
+			"id": "662d4e4cfdcae00021e1b8bb",
+			"seo": {
+				"og:image": "https://uploads.quarkly.io/5f44d0da669357001e60ed14/images/website-example-quarkly.png?v=2020-11-05T19:48:01.806Z",
+				"title": "Work | Website Example",
+				"og:title": "Work | Website Example",
+				"description": "It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something bigger. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference.",
+				"og:description": "It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something bigger. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference."
+			},
+			"pageUrl": "work",
+			"name": "Work"
+		},
+		"662d4e4cfdcae00021e1b8bf": {
+			"id": "662d4e4cfdcae00021e1b8bf",
+			"seo": {
+				"description": "It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something bigger. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference.",
+				"og:description": "It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something bigger. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference.",
+				"og:image": "https://uploads.quarkly.io/5f44d0da669357001e60ed14/images/website-example-quarkly.png?v=2020-11-05T19:48:01.806Z",
+				"title": "Team | Website Example",
+				"og:title": "Team | Website Example"
+			},
+			"pageUrl": "team",
+			"name": "Team"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
